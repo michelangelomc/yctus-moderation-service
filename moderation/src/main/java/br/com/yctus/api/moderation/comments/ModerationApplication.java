2922e80273026e3a9b0ef70d7d386bc425cc48d2
michelangelomc <mickelchaves@hotmail.com>
2026-02-11 21:22:34 -0300

    start project

---
+++ moderation/src/main/java/br/com/yctus/api/moderation/comments/ModerationApplication.java
@@ -0,0 +1,13 @@
+package br.com.yctus.api.moderation.comments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModerationApplication.class, args);
+	}
+
+}
---
+++ moderation/src/test/java/br/com/yctus/api/moderation/comments/ModerationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yctus.api.moderation.comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
